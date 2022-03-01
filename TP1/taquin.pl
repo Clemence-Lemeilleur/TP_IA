49f3ebee21c762fcd5e3edb44d1a42d0093afc5e
Lemeilleur Clemence <clemence.lemeilleur@icloud.com>
2022-03-01 16:40:45 +0100

    yo

--- TP1/taquin.pl
+++ TP1/taquin.pl
@@ -2,13 +2,13 @@
 
 Doit contenir au moins 4 predicats qui seront utilises par A*
 
-   etat_initial(I)                                         % definit l'etat initial
+   etat_initial(I)                                         % definit letat initial
 
-   etat_final(F)                                           % definit l'etat final  
+   etat_final(F)                                           % definit letat final  
 
    rule(Rule_Name, Rule_Cost, Before_State, After_State)   % règles applicables
 
-   heuristique(Current_State, Hval)				           % calcul de l'heuristique 
+   heuristique(Current_State, Hval)				           % calcul de lheuristique 
 
 
 Les autres prédicats sont spécifiques au Taquin.
@@ -78,7 +78,7 @@ final_state([[a, b,  c],
 
 			 
    %********************
-   % AFFICHAGE D'UN ETAT
+   % AFFICHAGE D UN ETAT
    %********************
    % format :  write_state(?State) ou State est une liste de lignes a afficher
 
@@ -136,7 +136,7 @@ vertical_permutation(X,Y,S1,S2) :-
    append(Above, [Line3,Line4|Below], S2). % recompose S2 
 
    %***********************************************************************
-   % Retrait d'une occurrence X en position N dans une liste L (resultat R) 
+   % Retrait d une occurrence X en position N dans une liste L (resultat R) 
    %***********************************************************************
    % use case 1 :   delete(?N,?X,+L,?R)
    % use case 2 :   delete(?N,?X,?L,+R)   
@@ -146,76 +146,84 @@ delete(N,X,[Y|L], [Y|R]) :-
    delete(N1,X,L,R),
    N is N1 + 1.
 
-   
-   
-   %*******************
-   % PARTIE A COMPLETER
-   %*******************
-   
-   %*******************************************************************
-   % Coordonnees X(colonne),Y(Ligne) d'une piece P dans une situation U
-   %*******************************************************************
-	% format : coordonnees(?Coord, +Matrice, ?Element)
-	% Définit la relation entre des coordonnees [Ligne, Colonne] et un element de la matrice
-	/*
-	Exemples
-	
-	?- coordonnees(Coord, [[a,b,c],[d,e,f]],  e).        % quelles sont les coordonnees de e ?
-	Coord = [2,2]
-	yes
-	
-	?- coordonnees([2,3], [[a,b,c],[d,e,f]],  P).        % qui a les coordonnees [2,3] ?
-	P=f
-	yes
-	*/
 
-	
-	coordonnees([L,C], Mat, Elt) :- true.    %********
-											 % A FAIRE
-											 %********
 
-											 
-   %*************
-   % HEURISTIQUES
-   %*************
+%*******************
+% PARTIE A COMPLETER
+%*******************
+
+%*******************************************************************
+% Coordonnees X(colonne),Y(Ligne) d une piece P dans une situation U
+%*******************************************************************
+% format : coordonnees(?Coord, +Matrice, ?Element)
+% Définit la relation entre des coordonnees [Ligne, Colonne] et un element de la matrice
+/*
+Exemples
+
+?- coordonnees(Coord, [[a,b,c],[d,e,f]],  e).        % quelles sont les coordonnees de e ?
+Coord = [2,2]
+yes
+
+?- coordonnees([2,3], [[a,b,c],[d,e,f]],  P).        % qui a les coordonnees [2,3] ?
+P=f
+yes
+*/
+
+
+coordonnees([L,C], Mat, Elt) :- 
+   nth1(L,Mat,Ligne), nth1(C,Ligne, Elt).
+
+                                 
+%*************
+% HEURISTIQUES
+%*************
    
 heuristique(U,H) :-
-    heuristique1(U, H).  % au debut on utilise l'heuristique 1 
-%   heuristique2(U, H).  % ensuite utilisez plutot l'heuristique 2  
+    heuristique1(U, H).  % au debut on utilise l heuristique 1 
+%   heuristique2(U, H).  % ensuite utilisez plutot l heuristique 2  
    
    
-   %****************
-   %HEURISTIQUE no 1
-   %****************
-   % Nombre de pieces mal placees dans l'etat courant U
-   % par rapport a l'etat final F
-   
-   
-   % Suggestions : définir d'abord le prédicat coordonnees(Piece,Etat,Lig,Col) qui associe à une pièce présente dans Etat
-   % ses coordonnees (Lig= numero de ligne, Col= numero de Colonne)
-   
-   % Definir ensuite le predicat malplace(P,U,F) qui est vrai si les coordonnes de P dans U et dans F sont differentes.
-   % On peut également comparer les pieces qui se trouvent aux mêmes coordonnees dans U et dans H et voir s'il sagit de la
-   % même piece.
-   
-    % Definir enfin l'heuristique qui détermine toutes les pièces mal placées (voir prédicat findall) 
-	% et les compte (voir prédicat length)
-   
-    heuristique1(U, H) :- true.     %********
-                                    % A FAIRE
-                                    %********
-   
-   
-   %****************
-   %HEURISTIQUE no 2
-   %****************
-   
-   % Somme des distances de Manhattan à parcourir par chaque piece
-   % entre sa position courante et sa positon dans l'etat final
+%****************
+%HEURISTIQUE no 1
+%****************
+% Nombre de pieces mal placees dans l etat courant U
+% par rapport a l etat final F
 
-   
-    heuristique2(U, H) :- true.     %********
-                                    % A FAIRE
-                                    %********
-									
+
+% Suggestions : définir d abord le prédicat coordonnees(Piece,Etat,Lig,Col) qui associe à une pièce présente dans Etat
+% ses coordonnees (Lig= numero de ligne, Col= numero de Colonne)
+
+% Definir ensuite le predicat malplace(P,U,F) qui est vrai si les coordonnes de P dans U et dans F sont differentes.
+% On peut également comparer les pieces qui se trouvent aux memes coordonnees dans U et dans H et voir s il sagit de la
+% meme piece.
+
+   % Definir enfin l heuristique qui détermine toutes les pièces mal placées (voir prédicat findall) 
+% et les compte (voir prédicat length)
+
+malplace(P, U, Final):-
+   nth1(L,U,Ligne), nth1(C,Ligne,P), nth1(L,Final,LigneF), 
+   nth1(C,LigneF,M), P\=M, P\=vide.
+
+heuristique1(U, H) :- 
+   final_state(Final), findall(P, malplace(P, U,Final), Res), 
+   length(Res, H).
+
+
+
+%****************
+%HEURISTIQUE no 2
+%****************
+
+% Somme des distances de Manhattan à parcourir par chaque piece
+% entre sa position courante et sa positon dans letat final
+
+distMan(P, Dist)
+   //finir euristique 2//
+heuristique2(U, H) :-
+   final_state(Final), coordonnees([Lu, Cu], U, Pu),
+   coordonnees([Lf, Cf], Final, Pf), Pu=Pf,
+   findall( (abs(Lu-Lf) + abs(Cu-Cf)) , Pu\=vide, R),
+   sumlist(R,H).
+
+
 									
